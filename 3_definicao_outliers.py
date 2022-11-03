f4ca85e41c82679f9079758d9360044c33a8c7e2
Rodrigo Formigon <rodris.hud@gmail.com>
2022-11-03 00:22:13 -0300

    Update 3_definicao_outliers.py

--- 3_definicao_outliers.py
+++ 3_definicao_outliers.py
@@ -26,3 +26,37 @@ def limites(coluna):
     return q1 - 1.5 * amplitude, q3 + 1.5 * amplitude
 
 print(limites(base_airbnb['price']))
+
+
+# Plotando um gráfico
+def diagrama_caixa(coluna):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(15,5)
+    sns.boxplot(x=coluna, ax=ax1)
+    ax2.set_xlim(limites(coluna))
+    sns.boxplot(x=coluna, ax=ax2)
+    
+    
+# Criando gráfico de histograma
+def histograma(coluna):
+    plt.figure(figsize=(15,5))
+    sns.distplot(coluna, hist=True)
+    
+
+# Como estamos construindo um modelo para imóveis comuns, acredito que os valores acima do limite superior serão apenas de apartamentos de altíssimo luxo, que não é o nosso objetivo principal. Por isso, podemos excluir esses outliers.
+
+# Função para excluir os outliers da base de dados
+def excluir_outliers(df, nome_coluna):
+    qtde_linhas = df.shape[0]
+    lim_inf, lim_sup = limites(df[nome_coluna])
+    
+    df = df.loc[(df[nome_coluna] >= lim_inf) & (df[nome_coluna] <= lim_sup), :]
+    linhas_removidas = qtde_linhas - df.shape[0]
+    
+    return df, linhas_removidas
+
+base_airbnb, linhas_removidas = excluir_outliers(base_airbnb, 'price')
+print(f'{linhas_removidas} linhas removidas')
+
+diagrama_caixa(base_airbnb['price'])
+histograma(base_airbnb['price'])
